7c3a6e6064fd9af4801660d2df5345e81fe0be5f
Ashutosh Rajput <85149152+aashut0xh@users.noreply.github.com>
2023-06-15 10:39:48 +0530

    10:39 15/06/2023

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"Enter Two number:";
+cin>>a>>b;
+c=a+b;
+cout<<"The sum of Number is: " <<c<<endl;
+c=a-b;
+cout<<"The Difference of Number is: "<<c<<endl;
+c=a*b;
+cout<<"The Mulatiplication of Number is: "<<c<<endl;
+c=a/b;
+cout<<"The Division of Number is: "<<c<<endl;
+return 0;
+}
