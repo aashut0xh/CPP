edb1f25198308c7d9c0745895062a10d0c562b6e
Ashutosh Rajput <85149152+aashut0xh@users.noreply.github.com>
2023-06-24 20:09:43 +0530

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[4]= {23,67,87,25};
+    int i=0;
+  //  while(i<=3)
+   // {
+   //     cout<<marks[i]<<endl;
+  //      i++;
+   // }
+   do
+   {
+     cout<<marks[i]<<endl;
+     i++;
+   } while (i<=3);
+   
+    
+   
+    
+    
+
+
+return 0;
+}
